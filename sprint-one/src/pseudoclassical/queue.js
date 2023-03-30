28031b62838e602447ad6f9ec102ae2e2fa040fe
Alex Caron <alex.caron@gmail.com>
2023-03-30 08:24:26 -0500

    create pseudoclassical version of queue data structure

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.last = 0;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.last += 1;
+  this.storage[this.last] = value;
+};
+
+Queue.prototype.dequeue = function() {
+  var valueToDequeue = this.storage['1'];
+  for (var key = 1; key < this.last; key++) {
+    this.storage[key] = this.storage[key + 1];
+  }
+  if (this.last !== 0) {
+    this.last -= 1;
+  }
+  return valueToDequeue;
+};
+
+Queue.prototype.size = function() {
+  return this.last;
 };
 
 
