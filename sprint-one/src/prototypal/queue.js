fe58c7288c136aab9edc110acda52aa3f5944cbb
Alex Caron <alex.caron@gmail.com>
2023-03-30 08:20:16 -0500

    create prototypal pattern version of queue data structure

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  instance.storage = {};
+  instance.last = 0;
+  return instance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.last += 1;
+  this.storage[this.last] = value;
+};
+
+queueMethods.dequeue = function() {
+  var valueToDequeue = this.storage['1'];
+  for (var key = 1; key < this.last; key ++) {
+    this.storage[key] = this.storage[key + 1];
+  }
+  if (this.last !== 0) {
+    this.last -= 1;
+  }
+  return valueToDequeue;
+};
+
+queueMethods.size = function() {
+  return this.last;
+};
 
 
